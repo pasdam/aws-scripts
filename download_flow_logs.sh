3d8a3df9c56639138889918dd6fff35dbdfecbd7
pasdam <pasdam@users.noreply.github.com>
2022-12-02 19:51:20 +0100

    Update script to download flow logs

--- download_flow_logs.sh
+++ download_flow_logs.sh
@@ -2,13 +2,14 @@
 
 print_usage() {
   SCRIPT_NAME=$(basename "$0")
-  echo "Usage: $SCRIPT_NAME <aws-profile> <log-group> <from> <to>"
+  echo "Usage: $SCRIPT_NAME <aws-profile> <log-group> <from> <to> [<output-dir>]"
   echo "Example: $SCRIPT_NAME"
   echo "Parameters:"
   echo "  aws-profile  AWS profile to use"
   echo "  log-group    Name of the log group to download"
   echo "  from         Timestamp (Unix epoch in milliseconds) of the date and time from which download the logs"
   echo "  to           Timestamp (Unix epoch in milliseconds) of the date and time up to which download the logs"
+  echo "  output-dir   Directory where store downloaded logs"
   exit "$1"
 }
 
@@ -36,36 +37,40 @@ if [ -z "$TO" ]; then
   exit 4
 fi
 
+OUTPUT_DIR=$5
+if [ -z "$OUTPUT_DIR" ]; then
+  OUTPUT_DIR=.
+fi
 
 export AWS_PAGER="" # Disable pagination
 export AWS_REGION=me-south-1
 
-INSTANCES=$(aws ec2 describe-instances --filters Name=instance-state-code,Values=16 | jq '[.Reservations[] | .Instances[]]')
-while IFS= read -r instance_id; do
-  INTERFACES=$(echo "$INSTANCES" | jq '[.[] | select(.InstanceId == "'"$instance_id"'") | .NetworkInterfaces[]]')
+echo "AWS profile to use: $AWS_PROFILE"
+echo "Log group to download: $LOG_GROUP"
+echo "From: $(date -r $(($FROM / 1000))) ($(date -u -r $(($FROM / 1000))))"
+echo "To: $(date -r $(($TO / 1000))) ($(date -u -r $(($TO / 1000))))"
+echo "Output directory: $OUTPUT_DIR"
 
-  while IFS= read -r interface_id; do
+mkdir -p $OUTPUT_DIR
 
-    LOG_FILE="./${instance_id}_${interface_id}_${FROM}.log"
+LOG_STREAMS=$(aws logs describe-log-streams --log-group-name "$LOG_GROUP")
+while IFS= read -r log_stream_name; do
+  echo "Processing log stream: $log_stream_name"
+
+    LOG_FILE="${OUTPUT_DIR}/${log_stream_name}_${FROM}_${TO}.log"
     echo -n "" > "$LOG_FILE"
-
 
     LAST_TOKEN="last"
     CURRENT_TOKEN=""
     while [ "$LAST_TOKEN" != "$CURRENT_TOKEN" ]; do
       LAST_TOKEN=$CURRENT_TOKEN
-      echo "Processing token: $CURRENT_TOKEN"
+      printf "\tProcessing token: %s\n" "$CURRENT_TOKEN"
       if [ -n "$CURRENT_TOKEN" ]; then
         CURRENT_TOKEN="--next-token $CURRENT_TOKEN"
       fi
-      LOGS=$(aws logs get-log-events $CURRENT_TOKEN --log-group-name "$LOG_GROUP" --log-stream-name "$interface_id"-all --start-time "$FROM" --end-time "$TO")
+      LOGS=$(aws logs get-log-events $CURRENT_TOKEN --log-group-name "$LOG_GROUP" --log-stream-name "$log_stream_name" --start-time "$FROM" --end-time "$TO")
       CURRENT_TOKEN=$(echo "$LOGS" | jq -r '.nextForwardToken')
-      echo "$LOGS" | jq -c -r '.events[] | .message' | sed "s/$INTERFACE_NAME //" >> "$LOG_FILE"
+      echo "$LOGS" | jq -c -r '.events[] | .message' >> "$LOG_FILE"
     done
 
-  done < <(echo "$INTERFACES" | jq -c -r '.[] | .NetworkInterfaceId')
-done < <(echo "$INSTANCES" | jq -c -r '.[] | .InstanceId')
-exit 0
-
-echo "Log stream name: $LOG_STREAM"
-echo "Interface name: $INTERFACE_NAME"
+done < <(echo "$LOG_STREAMS" | jq -c -r '.logStreams[].logStreamName')
